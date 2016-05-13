6d7bb20c69a830ba8cecd766859c806d74091378
Dejan Lavbič <Dejan@lavbic.net>
2016-05-13 11:01:29 +0200

    Začetna verzija repozitorija

---
+++ knjiznice/js/koda.js
@@ -0,0 +1,42 @@
+
+var baseUrl = 'https://rest.ehrscape.com/rest/v1';
+var queryUrl = baseUrl + '/query';
+
+var username = "ois.seminar";
+var password = "ois4fri";
+
+
+/**
+ * Prijava v sistem z privzetim uporabnikom za predmet OIS in pridobitev
+ * enolične ID številke za dostop do funkcionalnosti
+ * @return enolični identifikator seje za dostop do funkcionalnosti
+ */
+function getSessionId() {
+    var response = $.ajax({
+        type: "POST",
+        url: baseUrl + "/session?username=" + encodeURIComponent(username) +
+                "&password=" + encodeURIComponent(password),
+        async: false
+    });
+    return response.responseJSON.sessionId;
+}
+
+
+/**
+ * Generator podatkov za novega pacienta, ki bo uporabljal aplikacijo. Pri
+ * generiranju podatkov je potrebno najprej kreirati novega pacienta z
+ * določenimi osebnimi podatki (ime, priimek in datum rojstva) ter za njega
+ * shraniti nekaj podatkov o vitalnih znakih.
+ * @param stPacienta zaporedna številka pacienta (1, 2 ali 3)
+ * @return ehrId generiranega pacienta
+ */
+function generirajPodatke(stPacienta) {
+  ehrId = "";
+
+  // TODO: Potrebno implementirati
+
+  return ehrId;
+}
+
+
+// TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
